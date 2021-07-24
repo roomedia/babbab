1b5d1e48d591677245bbef181f5505e46cbe8eb9
Dongho Kim <k45414254@gmail.com>
2021-07-25 01:09:56 +0900

    login activity

---
+++ app/src/main/java/com/roomedia/bobbob/ui/login/ActivityLogin.kt
@@ -0,0 +1,65 @@
+package com.roomedia.bobbob.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.roomedia.bobbob.extension.startActivity
+import com.roomedia.bobbob.extension.startActivityForResult
+import com.roomedia.bobbob.ui.main.ActivityMain
+import timber.log.Timber
+
+class ActivityLogin : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createSignInIntent()
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),)
+
+        AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+//            .setLogo(R.drawable.my_great_logo) // Set logo drawable
+//            .setTheme(R.style.MySuperAppTheme) // Set theme
+            .build()
+            .startActivityForResult(signInLauncher)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+//            val user = FirebaseAuth.getInstance().currentUser
+            startActivity(ActivityMain::class.java)
+            return
+        }
+        Timber.w(response?.error)
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // ...
+            }
+    }
+
+    private fun delete() {
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+                // ...
+            }
+    }
+}
---
+++ app/src/main/java/com/roomedia/bobbob/ui/main/ActivityMain.kt
@@ -0,0 +1,7 @@
+package com.roomedia.bobbob.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityMain : AppCompatActivity() {
+
+}
