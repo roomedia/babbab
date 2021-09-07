306589acd63e3f22b86c76aa0f80e953ebda1e1f
Dongho Kim <roomedia@naver.com>
2021-09-07 21:35:42 +0900

    Add Firebase Emulators

--- app/src/main/java/com/roomedia/babbab/MyApplication.kt
+++ app/src/main/java/com/roomedia/babbab/MyApplication.kt
@@ -1,6 +1,8 @@
 package com.roomedia.babbab
 
 import android.app.Application
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 
 class MyApplication : Application() {
@@ -8,6 +10,12 @@ class MyApplication : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+
+            // When running in debug mode, connect to the Firebase Emulator Suite.
+            // "10.0.2.2" is a special IP address which allows the Android Emulator
+            // to connect to "localhost" on the host computer. The port values (9xxx)
+            // must match the values defined in the firebase.json file.
+            Firebase.auth.useEmulator("10.0.2.2", 9099)
         }
     }
 }
--- app/src/main/java/com/roomedia/babbab/ui/login/LoginActivity.kt
+++ app/src/main/java/com/roomedia/babbab/ui/login/LoginActivity.kt
@@ -1,65 +1,71 @@
 package com.roomedia.babbab.ui.login
 
-import android.os.Bundle
+import android.app.Activity
+import android.content.Intent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.roomedia.babbab.extension.startActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.roomedia.babbab.R
 import com.roomedia.babbab.extension.startActivityForResult
-import com.roomedia.babbab.ui.main.MainActivity
 import timber.log.Timber
 
 class LoginActivity : AppCompatActivity() {
 
-    private val signInLauncher = registerForActivityResult(
-        FirebaseAuthUIActivityResultContract()
-    ) { res ->
-        this.onSignInResult(res)
-    }
+    private val signInLauncher =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract(), ::onSignInResult)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        createSignInIntent()
-    }
-
-    private fun createSignInIntent() {
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),)
-
-        AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-//            .setLogo(R.drawable.my_great_logo) // Set logo drawable
-//            .setTheme(R.style.MySuperAppTheme) // Set theme
-            .build()
-            .startActivityForResult(signInLauncher)
+    override fun onStart() {
+        super.onStart()
+        if (Firebase.auth.currentUser == null) {
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setLogo(R.mipmap.ic_launcher)
+                .setAvailableProviders(listOf(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build(),
+//                    AuthUI.IdpConfig.TwitterBuilder().build(),
+//                    AuthUI.IdpConfig.FacebookBuilder().build(),
+//                    AuthUI.IdpConfig.NaverBuilder().build(),
+//                    AuthUI.IdpConfig.KakaoBuilder().build(),
+                ))
+                .build()
+            signInLauncher.launch(signInIntent)
+        } else {
+            startPreviousActivity()
+        }
     }
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
-//            val user = FirebaseAuth.getInstance().currentUser
-            startActivity(MainActivity::class.java)
-            return
+            Timber.d("Sign in successful!")
+            startPreviousActivity()
+        } else {
+            Toast.makeText(this, R.string.sign_in_error, Toast.LENGTH_LONG).show()
+            val response = result.idpResponse
+            if (response == null) {
+                Timber.w("Sign in canceled")
+            } else {
+                Timber.w("Sign in error", response.error)
+            }
         }
-        Timber.w(response?.error)
     }
 
-    private fun signOut() {
-        AuthUI.getInstance()
-            .signOut(this)
-            .addOnCompleteListener {
-                // ...
-            }
+    private fun startPreviousActivity() {
+        intent.getStringExtra(KEY_ACTIVITY_NAME)?.also { activityName ->
+            startActivity(Intent(this, Class.forName(activityName)))
+            finish()
+        }
     }
 
-    private fun delete() {
-        AuthUI.getInstance()
-            .delete(this)
-            .addOnCompleteListener {
-                // ...
-            }
+    companion object {
+        private const val KEY_ACTIVITY_NAME = "activity_name"
+
+        fun createIntent(activity: Activity): Intent =
+            Intent(activity, LoginActivity::class.java)
+                .putExtra(KEY_ACTIVITY_NAME, activity::class.qualifiedName)
     }
 }
